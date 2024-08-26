0c4a2e7051de43a295724fd6e13add4b475a7386
Adrian Ramos <adrianrmz917@gmail.com>
2024-08-25 22:16:59 -0600

    Added App and Server files and updated index.php Secutiry parameters added

---
+++ config/App.php
@@ -0,0 +1,6 @@
+<?php
+const SERVERURL = "http://localhost/PHP-MVC/";
+const COMPANY = "Loan System";
+const CURRENCY = "$";
+date_default_timezone_set('America/Mexico_City');
+
---
+++ config/Server.php
@@ -0,0 +1,13 @@
+<?php
+const SERVER = "localhost";
+const DB = "loan_system";
+const USER = "root";
+const PASS = "admin";
+
+const SGBD = "mysql:host=".SERVER.";dbname=".DB;
+
+const METHOD = "AES-256-CBC";
+const SECRET_KEY = '$LOAN@2021';
+const SECRET_IV = "747391"
+
+
